349f9ee5fcd6d99b7d70a185d62b8f935615bd80
IamJoeBiden <etzbacherelias@gmail.com>
2025-10-06 11:41:15 +0200

    Asset

---
+++ XSlicer/Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0.01f, 0, 0) * Time.deltaTime * speed);
+    }
+}
---
+++ XSlicer/Assets/Scripts/SongBehaviour.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SongBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private string songName;
+    [SerializeField]
+    private int bpm;
+    [SerializeField]
+    private GameObject cube;
+    private float timeCount;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timeCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCount += Time.deltaTime;
+        if((60/bpm) - timeCount <= 0.001f){
+            timeCount = 0;
+            Instantiate(cube, transform.position, Quaternion.identity);
+        }
+    }
+}
