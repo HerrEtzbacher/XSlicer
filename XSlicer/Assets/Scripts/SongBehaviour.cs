a8e6a37b3c3becfac26e128ef4d589adf6bcf298
IamJoeBiden <etzbacherelias@gmail.com>
2025-10-20 11:13:56 +0200

    SongBehaviourFINISHED

--- XSlicer/Assets/Scripts/SongBehaviour.cs
+++ XSlicer/Assets/Scripts/SongBehaviour.cs
@@ -1,23 +1,34 @@
+using System;
 using System.Collections;
-using System.IO;
 using UnityEngine;
 
 public class SongBehaviour : MonoBehaviour
 {
-    private FastAPIClient aPIClient;
 
     [SerializeField]
     private string songName;
 
     [SerializeField]
-    private int bpm;
+    private float bpm;
+
     [SerializeField]
     private GameObject cube;
     private float timeCount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+    IEnumerator InstantiateCube()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(60/bpm);
+            Instantiate(cube, transform.position, Quaternion.identity);
+        }
+        
+    }
     void Start()
     {
-        FastAPIClient.Instance.ProcessSong("https://www.youtube.com/watch?v=14954yE101g&list=RDlPGipwoJiOM&index=4", (songData) =>
+        Debug.Log("Test");
+        FastAPIClient.Instance.ProcessSong("https://www.youtube.com/watch?v=Zv8czIoAw5w", (songData) =>
         {
             if (songData == null)
             {
@@ -26,47 +37,19 @@ public class SongBehaviour : MonoBehaviour
             }
 
             Debug.Log(songData.rhythm_analysis.tempo_bpm);
-            StartCoroutine(DownloadAndCacheSong(songData));
 
-            Debug.Log($"New song ready: {songData.title}, ID: {songData.id}");
-            Debug.Log($"Cached at: {songData.localPath}");
-        });
-        timeCount = 0;
+            Debug.Log("Processed");
+            bpm = (float)songData.rhythm_analysis.tempo_bpm;
+            Debug.Log("Beats per minute" + bpm);
+
+            StartCoroutine(InstantiateCube());
+        }); 
     }
     
-    private IEnumerator DownloadAndCacheSong(SongData song)
-    {
-        string songsDir = Path.Combine(Application.persistentDataPath, "Songs");
-        string songFolder = Path.Combine(songsDir, song.id);
-        Directory.CreateDirectory(songFolder);
-
-        bool metadataDone = false;
-        bool fileDone = false;
-
-        string metadataPath = Path.Combine(songFolder, $"{song.id}.json");
-        File.WriteAllText(metadataPath, JsonUtility.ToJson(song, true));
-        metadataDone = true;
-
-        FastAPIClient.Instance.DownloadSongFile(song.id, (filePath) =>
-        {
-            string destPath = Path.Combine(songFolder, $"{song.id}.mp3");
-            if (filePath != destPath)
-                File.Copy(filePath, destPath, true);
-            song.localPath = destPath;
-            fileDone = true;
-        });
-
-        yield return new WaitUntil(() => metadataDone && fileDone);
-        Debug.Log($"Song {song.id} cached locally.");
-    }
 
     // Update is called once per frame
     void Update()
-    {
-        timeCount += Time.deltaTime;
-        if((60/bpm) - timeCount <= 0.001f){
-            timeCount = 0;
-            Instantiate(cube, transform.position, Quaternion.identity);
-        }
+    {    
+        
     }
 }
