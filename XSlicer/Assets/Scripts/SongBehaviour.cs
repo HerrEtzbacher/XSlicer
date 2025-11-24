843033499197b76fe9448311e36dd1f18da311d6
IamJoeBiden <etzbacherelias@gmail.com>
2025-11-24 10:48:28 +0100

    SpawningBlockAndTime

--- XSlicer/Assets/Scripts/SongBehaviour.cs
+++ XSlicer/Assets/Scripts/SongBehaviour.cs
@@ -1,6 +1,12 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
+using UnityEngine.LightTransport;
+
+
 
 public class SongBehaviour : MonoBehaviour
 {
@@ -12,17 +18,28 @@ public class SongBehaviour : MonoBehaviour
     private float bpm;
 
     [SerializeField]
-    private List<Transform> speedyThings;
+    private List<GameObject> speedyThings;
+
+    [SerializeField]
+    private List<Transform> positions;
+
+    private System.Random random;
 
     [SerializeField]
     private GameObject cube;
     private float timeCount;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
+
+    private float videoLength;   
+    [SerializeField] TextMeshPro scoreText;
+
+    private bool isProcessing = true;
+
+    private AudioSource audioSource;
     IEnumerator InstantiateCube()
     {
-        while (true)
+        while (videoLength > float.Parse(string.Format("{0:F2}", timeCount), CultureInfo.InvariantCulture.NumberFormat))
         {
+            
             yield return new WaitForSeconds(60 / bpm);
             int randy = random.Next(0, speedyThings.Count + 1);
             int randomRotation = random.Next(0, 360);
@@ -31,6 +48,7 @@ public class SongBehaviour : MonoBehaviour
         
                 Instantiate(speedyThings[0], positions[0].position, Quaternion.identity);
                 Instantiate(speedyThings[1], positions[1].position, Quaternion.identity);
+                
             }
             else
             {
@@ -39,11 +57,16 @@ public class SongBehaviour : MonoBehaviour
         }
         
     }
+    
     void Start()
     {
+        StartCoroutine(Loading());
+        random = new System.Random();
         Debug.Log("Test");
+        
         FastAPIClient.Instance.ProcessSong("https://www.youtube.com/watch?v=Zv8czIoAw5w", (songData) =>
         {
+            isProcessing = false;
             if (songData == null)
             {
                 Debug.LogError("Failed to process song.");
@@ -53,7 +76,10 @@ public class SongBehaviour : MonoBehaviour
             Debug.Log(songData.rhythm_analysis.tempo_bpm);
 
             Debug.Log("Processed");
+
             bpm = (float)songData.rhythm_analysis.tempo_bpm;
+            videoLength = (float)songData.duration;
+
             Debug.Log("Beats per minute" + bpm);
 
             StartCoroutine(InstantiateCube());
@@ -61,9 +87,30 @@ public class SongBehaviour : MonoBehaviour
     }
     
 
+    IEnumerator Loading()
+    {
+        while (isProcessing)
+        {
+            scoreText.text = "Verarbeite Lied.";
+            yield return new WaitForSeconds(0.5f);
+            scoreText.text = "Verarbeite Lied..";
+            yield return new WaitForSeconds(0.5f);
+            scoreText.text = "Verarbeite Lied...";
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
     // Update is called once per frame
     void Update()
     {    
-        
+       if(!isProcessing && timeCount < videoLength)
+       {
+           timeCount += Time.deltaTime / 100;
+            scoreText.text = "Zeit: " + float.Parse(string.Format("{0:F2}", timeCount), CultureInfo.InvariantCulture.NumberFormat) + " / " + float.Parse(string.Format("{0:F2}", videoLength), CultureInfo.InvariantCulture.NumberFormat)/100 ;
+        }
+        else if (!isProcessing)
+        {
+            scoreText.text = " LEVEL COMPLETE! (" + float.Parse(string.Format("{0:F2}", videoLength), CultureInfo.InvariantCulture.NumberFormat)/100 + "s) ";
+        }
     }
 }
